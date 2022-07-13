9cdca043bff89a798561a225b9600183c1c6826c
Edward Anderson <edward.anderson@probitas1492.com>
2022-07-13 13:46:23 +0100

    updated requirements

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from FlaskTemplate import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '3000'))
+        PORT = int(environ.get('SERVER_PORT', '80'))
     except ValueError:
-        PORT = 3000
+        PORT = 80
     app.run(HOST, PORT)
